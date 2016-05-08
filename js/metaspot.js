cf26a3d99fbb4853f99817382b16560ee143d1b0
Gerald Truong <gerald.truong@citron.tech>
2016-05-08 19:55:19 +0000

    Added webpack

---
+++ js/metaspot.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { combineReducers, createStore } from 'redux'
+
+class Metaspot extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+  
+  render () {
+    return (
+      <div>Hello world. I am Metaspot.</div>
+    )
+  }
+}
+
+if (typeof window === 'object') {
+  window.metaspot = new function() {
+    let it = this
+    
+    it.render = function (opts) {
+      if (opts.container) {
+        it.container = opts.container
+        ReactDOM.render(<Metaspot />, it.container)  
+      }
+    }
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var path = require('path');
+var process = require('process');
+var webpack = require('webpack');
+var extractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  context: __dirname,
+  devtool: 'inline-source-map',
+  entry: {
+    'metaspot': './js/metaspot.js'
+  },
+  output: {
+    publicPath: '/',
+    path: path.resolve(__dirname, 'build'),
+    sourceMapFilename: '[file].map',
+    filename: '[name].bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /.js/,
+        loader: 'babel-loader',
+        exclude: [/node_modules/],
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.less$/,
+        loader: extractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!less-loader')
+      },
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.(ttf|woff2|eot|woff|svg|png|gif|jpe?g)$/, loader: 'file-loader' }
+    ]
+  },
+  plugins: [
+    new extractTextPlugin("./[name].css"),
+    new webpack.DefinePlugin({
+      'process.env': { 'NODE_ENV': JSON.stringify('production') },
+    }),
+    new webpack.optimize.DedupePlugin()
+  ]
+};
