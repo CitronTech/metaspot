d5f499dd22b3aadbd3f36195666e850a00bd11d3
Gerald Truong <gerald.truong@citron.tech>
2016-11-26 14:11:06 +0000

    Removed ops scripts

--- js/lambdas/test/src/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-exports.handler = (event, context, callback) => {
-  console.log(event.i);
-  callback(null, { result: 'ok' });
-  //context.done(null, { result: 'hello world.' });
-}
