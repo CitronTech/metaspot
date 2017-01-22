4799d85d0090f20594af5fdf4a28afc024c52aa4
Gerald Truong <gerald.truong@citron.tech>
2017-01-22 01:45:22 +0000

    FE

--- js/metaspot.js
+++ js/metaspot.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM, { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { combineReducers, createStore, applyMiddleware } from 'redux'
+import Reducer from './reducer'
 import { Router, Route, Link, IndexRoute, browserHistory } from 'react-router'
 import App from './components/App'
 import About from './components/About'
 import css from '../styles.less'
+
+export const store = createStore(
+  combineReducers({
+    Reducer
+  })
+)
 
 if (typeof window === 'object') {
   window.metaspot = new function() {
@@ -15,15 +24,17 @@ if (typeof window === 'object') {
         it.browserHistory = browserHistory
         
         render((
-          <Router history={ browserHistory }>
-            <Route path="/">
-              <Route path="metaspot">
-                <IndexRoute component={ App } />
-                <Route path="about" component={ About }>
+          <Provider store={ store }>
+            <Router history={ browserHistory }>
+              <Route path="/">
+                <Route path="metaspot">
+                  <IndexRoute component={ App } />
+                  <Route path="about" component={ About }>
+                  </Route>
                 </Route>
               </Route>
-            </Route>
-          </Router>
+            </Router>
+          </Provider>
         ), it.container)  
       }
     }
