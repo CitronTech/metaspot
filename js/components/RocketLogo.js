049249e4836a9e2f8c34b12c62e85aea838ebaaf
Gerald Truong <gerald.truong@citron.tech>
2017-03-20 19:24:34 +0000

    Logo update

--- js/components/RocketLogo.js
+++ js/components/RocketLogo.js
@@ -7,6 +7,8 @@ class RocketLogo extends Component {
       	version="1.1" 
       	xmlns="http://www.w3.org/2000/svg"
       	x="0px" y="0px"
+      	width="40" 
+        height="40"
       	viewBox="0 0 330 330">
       	
       	<g>
